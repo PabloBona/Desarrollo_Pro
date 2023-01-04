b68fc027298a4848979fdd106e6df3c34db6cb32
fran <francisco.vento@outlook.com>
2023-01-04 10:22:26 -0300

    feat: using husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import styles from '../styles/Home.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  constate = false,
   return (
     <>
       <Head>
