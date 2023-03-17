ca7374e721a7e45387238b6e2b5934e9b73a17c6
PabloBona <110569486+PabloBona@users.noreply.github.com>
2023-03-16 23:19:52 -0300

    Header!

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,3 +1,11 @@
 export function Footer() {
-  return <div>FOOTER</div>;
+  return (
+    <div className='min-h-[488px] flex justify-center items-center flex-col bg-[url("/footer-banner.png")] bg-cover bg-center app-banner -mt-4 gap-5'>
+      <input
+        className="px-6 py-4 rounded-3xl w-full sm:w-[465px]"
+        type="text"
+        placeholder="¿Qué quieres ver en tu ciudad?"
+      ></input>
+    </div>
+  );
 }
